6b74211375ede9461cd30a294754c44e44ab261d
marcamarcelo <iamarcelomarques@gmail.com>
2024-12-20 02:33:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor em execução na porta ${PORT}`);
 });
+app.post('/webhook', (req, res) => {
+    console.log('Dados recebidos:', req.body);
+    res.status(200).send({ message: 'Webhook recebido com sucesso!' });
+});
