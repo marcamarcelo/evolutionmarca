aabab7e680e8bfb9557f20a941ca4458836c1989
marcamarcelo <iamarcelomarques@gmail.com>
2024-12-20 02:47:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ app.post('/webhook', (req, res) => {
     console.log('Dados recebidos:', req.body);
     res.status(200).send({ message: 'Webhook recebido com sucesso!' });
 });
+app.get('/config', (req, res) => {
+    res.send(`
+        <h1>Página de Configuração</h1>
+        <p>Bem-vindo à página de configuração da Evolution Marca!</p>
+    `);
+});
