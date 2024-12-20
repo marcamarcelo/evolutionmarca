8fd879d53ef3e987a6fb279c99a4dae5ecd901d5
marcamarcelo <iamarcelomarques@gmail.com>
2024-12-20 02:01:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, Evolution Marca!');
+    res.send('Olá, Evolution Marca!');
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Servidor em execução na porta ${PORT}`);
 });
